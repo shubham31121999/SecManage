9ff3653d69a60743e5683fbcd41c307ed843a046
Shubham Gajanan Uparkar <65241730+shubham31121999@users.noreply.github.com>
2024-08-20 07:05:18 +0530

    Done

--- security/config/wsgi.py
+++ security/config/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
 
 
-app = application
